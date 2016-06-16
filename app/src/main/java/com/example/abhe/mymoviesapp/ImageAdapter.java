f071d7b28dd92c0d8e5f4afe0dc26259f99f8e1b
abhe <cabheshek@gmail.com>
2016-06-16 18:04:42 +0530

    second commi

---
+++ app/src/main/java/com/example/abhe/mymoviesapp/ImageAdapter.java
@@ -0,0 +1,60 @@
+package com.example.abhe.mymoviesapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by abhe on 16-06-2016.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+    // Constructor
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
+        return imageView;
+    }
+
+}
--- app/src/main/java/com/example/abhe/mymoviesapp/MainActivity.java
+++ app/src/main/java/com/example/abhe/mymoviesapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.abhe.mymoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        my first one
+       GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+            // Instance of ImageAdapter Class
+            gridView.setAdapter(new ImageAdapter(this));
+
+
     }
 }
